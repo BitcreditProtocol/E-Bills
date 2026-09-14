BitcreditProtocol/Bitcredit-Core

Preserve advertised Blossom URLs

crates/bcr-ebill-api/src/service/bill_service/tests.rs
@@ -1,6 +1,6 @@
 use super::*;
 use crate::{
-    external::mint::QuoteStatusReply,
+    external::{file_storage::UploadedBlob, mint::QuoteStatusReply},
     service::{
         bill_service::test_utils::{MockBillContext, safe_deadline_ts},
         company_service::tests::{
@@ -64,6 +64,13 @@ use test_utils::{
     reject_buy_block, reject_recourse_block, reject_to_pay_block, request_to_accept_block,
     request_to_pay_block, request_to_recourse_block, sell_block,
 };
+
+fn uploaded_at(hash: Sha256HexHash, url: &str) -> UploadedBlob {
+    UploadedBlob {
+        hash,
+        url: url::Url::parse(url).unwrap(),
+    }
+}
 
 #[tokio::test]
 async fn get_bill_balances_baseline() {
@@ -383,10 +390,11 @@ async fn issue_bill_baseline() {
         .expect_remove_temp_upload_folder()
         .returning(|_| Ok(()));
     ctx.file_upload_client.expect_upload().returning(|_, _| {
-        Ok(nostr::hashes::sha256::Hash::from_str(
+        let hash = nostr::hashes::sha256::Hash::from_str(
             "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
         )
-        .unwrap())
+        .unwrap();
+        Ok(uploaded_at(hash, &format!("https://blossom.example/{hash}")))
     });
     ctx.bill_store.expect_save_keys().returning(|_, _| Ok(()));
     ctx.bill_store
@@ -458,10 +466,11 @@ async fn issue_bill_baseline_anon() {
         .expect_remove_temp_upload_folder()
         .returning(|_| Ok(()));
     ctx.file_upload_client.expect_upload().returning(|_, _| {
-        Ok(nostr::hashes::sha256::Hash::from_str(
+        let hash = nostr::hashes::sha256::Hash::from_str(
             "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
         )
-        .unwrap())
+        .unwrap();
+        Ok(uploaded_at(hash, &format!("https://blossom.example/{hash}")))
     });
     ctx.bill_store.expect_save_keys().returning(|_, _| Ok(()));
     ctx.bill_store
@@ -611,10 +620,11 @@ async fn issue_bill_as_company() {
         .expect_remove_temp_upload_folder()
         .returning(|_| Ok(()));
     ctx.file_upload_client.expect_upload().returning(|_, _| {
-        Ok(nostr::hashes::sha256::Hash::from_str(
+        let hash = nostr::hashes::sha256::Hash::from_str(
             "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
         )
-        .unwrap())
+        .unwrap();
+        Ok(uploaded_at(hash, &format!("https://blossom.example/{hash}")))
     });
     ctx.bill_store.expect_save_keys().returning(|_, _| Ok(()));
     ctx.bill_store
@@ -737,6 +747,10 @@ async fn upload_bill_files_for_node_id_reencrypts_for_receiver_key() {
     let expected_hash =
         Sha256HexHash::from_str("d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa")
             .unwrap();
+    let advertised_url = url::Url::parse(&format!(
+        "https://relay-public.example/blobs/{expected_hash}"
+    ))
+    .unwrap();
 
     ctx.identity_store
         .expect_get_current_identity()
@@ -756,14 +770,20 @@ async fn upload_bill_files_for_node_id_reencrypts_for_receiver_key() {
     let receiver_keys_for_signing = receiver_keys_for_upload.clone();
     ctx.file_upload_client
         .expect_upload()
-        .returning(move |_, bytes| {
-            let decrypted = bcr_ebill_core::protocol::crypto::decrypt_ecies(
-                &bytes,
-                &receiver_keys_for_upload.get_private_key(),
-            )
-            .unwrap();
-            assert_eq!(decrypted, plaintext_clone);
-            Ok(expected_hash)
+        .returning({
+            let advertised_url = advertised_url.clone();
+            move |_, bytes| {
+                let decrypted = bcr_ebill_core::protocol::crypto::decrypt_ecies(
+                    &bytes,
+                    &receiver_keys_for_upload.get_private_key(),
+                )
+                .unwrap();
+                assert_eq!(decrypted, plaintext_clone);
+                Ok(UploadedBlob {
+                    hash: expected_hash,
+                    url: advertised_url.clone(),
+                })
+            }
         })
         .once();
 
@@ -786,12 +806,7 @@ async fn upload_bill_files_for_node_id_reencrypts_for_receiver_key() {
         .await
         .unwrap();
 
-    assert_eq!(urls.len(), 1);
-    assert!(
-        urls[0]
-            .as_str()
-            .ends_with(expected_hash.to_string().as_str())
-    );
+    assert_eq!(urls, vec![advertised_url]);
 }
 
 #[tokio::test]

crates/bcr-ebill-api/src/service/company_service.rs
@@ -1932,7 +1932,10 @@ impl CompanyServiceApi for CompanyService {
 pub mod tests {
     use super::*;
     use crate::{
-        external::{email::MockEmailClientApi, file_storage::MockFileStorageClientApi},
+        external::{
+            email::MockEmailClientApi,
+            file_storage::{MockFileStorageClientApi, UploadedBlob, to_url},
+        },
         service::{
             contact_service::tests::{get_baseline_contact, get_baseline_nostr_contact},
             transport_service::MockTransportServiceApi,
@@ -1959,6 +1962,13 @@ pub mod tests {
     use mockall::predicate::eq;
     use nostr::hashes::sha256::Hash as Sha256HexHash;
     use std::{collections::HashMap, str::FromStr};
+
+    fn uploaded(server: &url::Url, hash: Sha256HexHash) -> UploadedBlob {
+        UploadedBlob {
+            hash,
+            url: to_url(server, &hash.to_string()).unwrap(),
+        }
+    }
 
     fn get_service(
         mock_storage: MockCompanyStoreApiMock,
@@ -2495,11 +2505,12 @@ pub mod tests {
         company_chain_store
             .expect_add_block()
             .returning(|_, _| Ok(()));
-        file_upload_client.expect_upload().returning(|_, _| {
-            Ok(nostr::hashes::sha256::Hash::from_str(
+        file_upload_client.expect_upload().returning(|server, _| {
+            let hash = nostr::hashes::sha256::Hash::from_str(
                 "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
             )
-            .unwrap())
+            .unwrap();
+            Ok(uploaded(server, hash))
         });
         storage.expect_save_key_pair().returning(|_, _| Ok(()));
         storage.expect_exists().returning(|_| false);
@@ -2786,11 +2797,12 @@ pub mod tests {
             .returning(|_| Ok(get_baseline_company_data().1.1));
         storage.expect_exists().returning(|_| true);
         storage.expect_update().returning(|_, _| Ok(()));
-        file_upload_client.expect_upload().returning(|_, _| {
-            Ok(nostr::hashes::sha256::Hash::from_str(
+        file_upload_client.expect_upload().returning(|server, _| {
+            let hash = nostr::hashes::sha256::Hash::from_str(
                 "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
             )
-            .unwrap())
+            .unwrap();
+            Ok(uploaded(server, hash))
         });
         identity_store.expect_get_full().returning(move || {
             let identity = empty_other_identity();
@@ -4130,11 +4142,12 @@ pub mod tests {
         file_upload_client
             .expect_upload()
             .times(1)
-            .returning(|_, _| {
-                Ok(nostr::hashes::sha256::Hash::from_str(
+            .returning(|server, _| {
+                let hash = nostr::hashes::sha256::Hash::from_str(
                     "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
                 )
-                .unwrap())
+                .unwrap();
+                Ok(uploaded(server, hash))
             });
 
         file_upload_client

crates/bcr-ebill-api/src/service/contact_service.rs
@@ -930,7 +930,7 @@ impl ContactServiceApi for ContactService {
 pub mod tests {
     use super::*;
     use crate::{
-        external::file_storage::MockFileStorageClientApi,
+        external::file_storage::{MockFileStorageClientApi, UploadedBlob, to_url},
         get_config,
         service::{
             Error, bill_service::test_utils::get_baseline_identity,
@@ -1325,11 +1325,15 @@ pub mod tests {
             .expect_read_temp_upload_file()
             .returning(|_| Ok((Name::new("avatar.png").unwrap(), vec![1, 2, 3])));
 
-        file_upload_client.expect_upload().returning(|_, _| {
-            Ok(nostr::hashes::sha256::Hash::from_str(
+        file_upload_client.expect_upload().returning(|server, _| {
+            let hash = nostr::hashes::sha256::Hash::from_str(
                 "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
             )
-            .unwrap())
+            .unwrap();
+            Ok(UploadedBlob {
+                hash,
+                url: to_url(server, &hash.to_string()).unwrap(),
+            })
         });
 
         file_reference_store.expect_get().returning(|_| Ok(None));

crates/bcr-ebill-api/src/service/file_reference_helper.rs
@@ -782,7 +782,7 @@ mod tests {
 
     #[tokio::test]
     async fn important_file_replication_triggers_when_missing_configured_server() {
-        use crate::external::file_storage::MockFileStorageClientApi;
+        use crate::external::file_storage::{MockFileStorageClientApi, UploadedBlob, to_url};
         use crate::tests::tests::MockFileReferenceStoreApiMock;
         use bitcoin::hashes::Hash;
 
@@ -819,7 +819,13 @@ mod tests {
 
         file_storage_client
             .expect_upload()
-            .returning(|_, _| Ok(nostr::hashes::sha256::Hash::from_slice(&[0u8; 32]).unwrap()));
+            .returning(|server, _| {
+                let hash = nostr::hashes::sha256::Hash::from_slice(&[0u8; 32]).unwrap();
+                Ok(UploadedBlob {
+                    hash,
+                    url: to_url(server, &hash.to_string()).unwrap(),
+                })
+            });
 
         file_reference_store
             .expect_add_server_urls()

BitcreditProtocol/E-Bills

crates/bcr-ebill-api/src/service/bill_service/tests.rs
@@ -1,6 +1,6 @@
 use super::*;
 use crate::{
-    external::mint::QuoteStatusReply,
+    external::{file_storage::UploadedBlob, mint::QuoteStatusReply},
     service::{
         bill_service::test_utils::{MockBillContext, safe_deadline_ts},
         company_service::tests::{
@@ -66,6 +66,13 @@ use test_utils::{
     reject_buy_block, reject_recourse_block, reject_to_pay_block, request_to_accept_block,
     request_to_pay_block, request_to_recourse_block, sell_block,
 };
+
+fn uploaded_at(hash: Sha256HexHash, url: &str) -> UploadedBlob {
+    UploadedBlob {
+        hash,
+        url: url::Url::parse(url).unwrap(),
+    }
+}
 
 #[tokio::test]
 async fn get_bill_balances_baseline() {
@@ -385,10 +392,11 @@ async fn issue_bill_baseline() {
         .expect_remove_temp_upload_folder()
         .returning(|_| Ok(()));
     ctx.file_upload_client.expect_upload().returning(|_, _| {
-        Ok(bitcoin::hashes::sha256::Hash::from_str(
+        let hash = bitcoin::hashes::sha256::Hash::from_str(
             "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
         )
-        .unwrap())
+        .unwrap();
+        Ok(uploaded_at(hash, &format!("https://blossom.example/{hash}")))
     });
     ctx.bill_store.expect_save_keys().returning(|_, _| Ok(()));
     ctx.bill_store
@@ -460,10 +468,11 @@ async fn issue_bill_baseline_anon() {
         .expect_remove_temp_upload_folder()
         .returning(|_| Ok(()));
     ctx.file_upload_client.expect_upload().returning(|_, _| {
-        Ok(bitcoin::hashes::sha256::Hash::from_str(
+        let hash = bitcoin::hashes::sha256::Hash::from_str(
             "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
         )
-        .unwrap())
+        .unwrap();
+        Ok(uploaded_at(hash, &format!("https://blossom.example/{hash}")))
     });
     ctx.bill_store.expect_save_keys().returning(|_, _| Ok(()));
     ctx.bill_store
@@ -613,10 +622,11 @@ async fn issue_bill_as_company() {
         .expect_remove_temp_upload_folder()
         .returning(|_| Ok(()));
     ctx.file_upload_client.expect_upload().returning(|_, _| {
-        Ok(bitcoin::hashes::sha256::Hash::from_str(
+        let hash = bitcoin::hashes::sha256::Hash::from_str(
             "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
         )
-        .unwrap())
+        .unwrap();
+        Ok(uploaded_at(hash, &format!("https://blossom.example/{hash}")))
     });
     ctx.bill_store.expect_save_keys().returning(|_, _| Ok(()));
     ctx.bill_store
@@ -739,6 +749,10 @@ async fn upload_bill_files_for_node_id_reencrypts_for_receiver_key() {
     let expected_hash =
         Sha256HexHash::from_str("d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa")
             .unwrap();
+    let advertised_url = url::Url::parse(&format!(
+        "https://relay-public.example/blobs/{expected_hash}"
+    ))
+    .unwrap();
 
     ctx.identity_store
         .expect_get_current_identity()
@@ -758,14 +772,20 @@ async fn upload_bill_files_for_node_id_reencrypts_for_receiver_key() {
     let receiver_keys_for_signing = receiver_keys_for_upload.clone();
     ctx.file_upload_client
         .expect_upload()
-        .returning(move |_, bytes| {
-            let decrypted = bcr_ebill_core::protocol::crypto::decrypt_ecies(
-                &bytes,
-                &receiver_keys_for_upload.get_private_key(),
-            )
-            .unwrap();
-            assert_eq!(decrypted, plaintext_clone);
-            Ok(expected_hash)
+        .returning({
+            let advertised_url = advertised_url.clone();
+            move |_, bytes| {
+                let decrypted = bcr_ebill_core::protocol::crypto::decrypt_ecies(
+                    &bytes,
+                    &receiver_keys_for_upload.get_private_key(),
+                )
+                .unwrap();
+                assert_eq!(decrypted, plaintext_clone);
+                Ok(UploadedBlob {
+                    hash: expected_hash,
+                    url: advertised_url.clone(),
+                })
+            }
         })
         .once();
 
@@ -788,12 +808,7 @@ async fn upload_bill_files_for_node_id_reencrypts_for_receiver_key() {
         .await
         .unwrap();
 
-    assert_eq!(urls.len(), 1);
-    assert!(
-        urls[0]
-            .as_str()
-            .ends_with(expected_hash.to_string().as_str())
-    );
+    assert_eq!(urls, vec![advertised_url]);
 }
 
 #[tokio::test]

crates/bcr-ebill-api/src/service/company_service.rs
@@ -1932,7 +1932,10 @@ impl CompanyServiceApi for CompanyService {
 pub mod tests {
     use super::*;
     use crate::{
-        external::{email::MockEmailClientApi, file_storage::MockFileStorageClientApi},
+        external::{
+            email::MockEmailClientApi,
+            file_storage::{MockFileStorageClientApi, UploadedBlob, to_url},
+        },
         service::{
             contact_service::tests::{get_baseline_contact, get_baseline_nostr_contact},
             transport_service::MockTransportServiceApi,
@@ -1959,6 +1962,13 @@ pub mod tests {
     use bitcoin::hashes::sha256::Hash as Sha256HexHash;
     use mockall::predicate::eq;
     use std::{collections::HashMap, str::FromStr};
+
+    fn uploaded(server: &url::Url, hash: Sha256HexHash) -> UploadedBlob {
+        UploadedBlob {
+            hash,
+            url: to_url(server, &hash.to_string()).unwrap(),
+        }
+    }
 
     fn get_service(
         mock_storage: MockCompanyStoreApiMock,
@@ -2495,11 +2505,12 @@ pub mod tests {
         company_chain_store
             .expect_add_block()
             .returning(|_, _| Ok(()));
-        file_upload_client.expect_upload().returning(|_, _| {
-            Ok(bitcoin::hashes::sha256::Hash::from_str(
+        file_upload_client.expect_upload().returning(|server, _| {
+            let hash = bitcoin::hashes::sha256::Hash::from_str(
                 "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
             )
-            .unwrap())
+            .unwrap();
+            Ok(uploaded(server, hash))
         });
         storage.expect_save_key_pair().returning(|_, _| Ok(()));
         storage.expect_exists().returning(|_| false);
@@ -2786,11 +2797,12 @@ pub mod tests {
             .returning(|_| Ok(get_baseline_company_data().1.1));
         storage.expect_exists().returning(|_| true);
         storage.expect_update().returning(|_, _| Ok(()));
-        file_upload_client.expect_upload().returning(|_, _| {
-            Ok(bitcoin::hashes::sha256::Hash::from_str(
+        file_upload_client.expect_upload().returning(|server, _| {
+            let hash = bitcoin::hashes::sha256::Hash::from_str(
                 "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
             )
-            .unwrap())
+            .unwrap();
+            Ok(uploaded(server, hash))
         });
         identity_store.expect_get_full().returning(move || {
             let identity = empty_other_identity();
@@ -4130,11 +4142,12 @@ pub mod tests {
         file_upload_client
             .expect_upload()
             .times(1)
-            .returning(|_, _| {
-                Ok(bitcoin::hashes::sha256::Hash::from_str(
+            .returning(|server, _| {
+                let hash = bitcoin::hashes::sha256::Hash::from_str(
                     "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
                 )
-                .unwrap())
+                .unwrap();
+                Ok(uploaded(server, hash))
             });
 
         file_upload_client

crates/bcr-ebill-api/src/service/contact_service.rs
@@ -930,7 +930,7 @@ impl ContactServiceApi for ContactService {
 pub mod tests {
     use super::*;
     use crate::{
-        external::file_storage::MockFileStorageClientApi,
+        external::file_storage::{MockFileStorageClientApi, UploadedBlob, to_url},
         get_config,
         service::{
             Error, bill_service::test_utils::get_baseline_identity,
@@ -1325,11 +1325,15 @@ pub mod tests {
             .expect_read_temp_upload_file()
             .returning(|_| Ok((Name::new("avatar.png").unwrap(), vec![1, 2, 3])));
 
-        file_upload_client.expect_upload().returning(|_, _| {
-            Ok(bitcoin::hashes::sha256::Hash::from_str(
+        file_upload_client.expect_upload().returning(|server, _| {
+            let hash = bitcoin::hashes::sha256::Hash::from_str(
                 "d277fe40da2609ca08215cdfbeac44835d4371a72f1416a63c87efd67ee24bfa",
             )
-            .unwrap())
+            .unwrap();
+            Ok(UploadedBlob {
+                hash,
+                url: to_url(server, &hash.to_string()).unwrap(),
+            })
         });
 
         file_reference_store.expect_get().returning(|_| Ok(None));

crates/bcr-ebill-api/src/service/file_reference_helper.rs
@@ -782,7 +782,7 @@ mod tests {
 
     #[tokio::test]
     async fn important_file_replication_triggers_when_missing_configured_server() {
-        use crate::external::file_storage::MockFileStorageClientApi;
+        use crate::external::file_storage::{MockFileStorageClientApi, UploadedBlob, to_url};
         use crate::tests::tests::MockFileReferenceStoreApiMock;
         use bitcoin::hashes::Hash;
 
@@ -819,7 +819,13 @@ mod tests {
 
         file_storage_client
             .expect_upload()
-            .returning(|_, _| Ok(bitcoin::hashes::sha256::Hash::from_slice(&[0u8; 32]).unwrap()));
+            .returning(|server, _| {
+                let hash = bitcoin::hashes::sha256::Hash::from_slice(&[0u8; 32]).unwrap();
+                Ok(UploadedBlob {
+                    hash,
+                    url: to_url(server, &hash.to_string()).unwrap(),
+                })
+            });
 
         file_reference_store
             .expect_add_server_urls()